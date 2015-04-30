4b99f38df65b30cc8c08265d34d12780bb6a3f7f
grepug <sk@grepug.com>
2015-04-30 10:05:39 +0800

    pb debug2

--- cloud/app.js
+++ cloud/app.js
@@ -36,6 +36,29 @@ app.post('/push', function(req, res) {
   })
 
 })
+app.get('/push', function(req, res) {
+  var hook = JSON.parse(req.param('hook'))
+  res.send(typeof hook)
+  request({
+    url: 'https://api.pushbullet.com/v2/pushes',
+    method: 'POST',
+    formData: {
+      "channel_tag": "coder",
+      "type": "note",
+      "title": "Repo Updated!(" + hook.commits.message + ")",
+      "body": hook.commits.message + " by" + hook.commits.author.name + "\n" + hook.commits.url
+    },
+    headers: {
+      'Authorization': 'Bearer 3dWeX1o53rCIHA71KwHamozU1DuFWu20',
+      'Content-Type': 'application/json'
+    }
+  }, function(err, res, body) {
+    if (!err && res.statusCode == 200) {
+      console.log(body)
+    } else console.log(body)
+  })
+
+})
 
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen()
